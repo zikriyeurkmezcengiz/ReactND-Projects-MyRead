0e5dec374d0f2c44c434b060ba83cc8c506e246a
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-09-07 22:01:49 +0300

    feat: created BookList

---
+++ src/utils/helpers/BookHelper.js
@@ -0,0 +1,13 @@
+const CategoryKeys = {
+  None: 0,
+  CurrentlyReading: 1,
+  WantToRead: 2,
+  Read: 3,
+};
+
+const CategoryDefinitions = {
+  [CategoryKeys.None]: "None",
+  [CategoryKeys.CurrentlyReading]: "Currenty Reading",
+  [CategoryKeys.WantToRead]: "Want To Read",
+  [CategoryKeys.Read]: "Read",
+};
